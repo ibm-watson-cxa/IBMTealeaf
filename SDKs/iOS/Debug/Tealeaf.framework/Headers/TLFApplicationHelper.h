574d3eb726dc960e7bf159a2163304252f425e18
Omar Hernandez <omar.hernandez@acoustic.co>
2021-04-15 20:12:41 -0700

    Beta Tealeaf build: 10_6_70
    Beta Tealeaf Change Notes:
    Enhancements
    
    - Adding support for SwiftUI.
    - Adding a flag SwiftUICaptureNonVariadic in TealeafAdvancedConfig.json to help capture all non Variadic pages from UIHostingController of SwiftUI. Default value is set to false.
    - Ability to turn off textChanged events to limit listening to user typing on textBox.
    
    Bug Fixes
    
    - Fixed an issue with backbutton not capturing correct page on gesture event. I am using upper left zone to assume backbutton is selected to use correct page.
    - Adjusted capture of inner views of WKWebview which covered replay of webview and appeared grayish white.
    - Added a flag 'AddGestureRecognizerUIScrollView' in TealeafAdvancedConfig.json to help with not auto enabling shouldRecognizeSimultaneouslyWithGestureRecognizer on UIScrollView which breaks the layout when you have a UIPageControl with UIScrollView. When you swipe left or right, the table also scrolls due to enabling shouldRecognizeSimultaneouslyWithGestureRecognizer to return YES.
    - Removed 'UIWebview' text that was being flagged by Apple.
    
    Known Issues
    
    - Captures gesture even when the gestureRecognizer is set to return 'NO', this is an Apple defect.
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - The pink boarder on Gestures screenview is off when captured using ER, because if timing or garbage collection.
    - Template issue - The back link is missing during capture on some screens Progressindicator, AlertViewController, Gestures screenview and picker view.
    - Template issue - Battery icon not visible in green anymore.
    - Improvement :- 'connectionType' remains N/A if device is on WiFi on app start.
    - DoPostOnScreenChange is not working as expected, because of temp queue.
    - Reporting of multiple carrier names in JSON if the device has Dual SIM is a beta feature.
    - React Native has an issue for dynamic builds. We will use static version instead.
    - Certain Alerts may appear correctly during replay.
    - If a View Controller has more than one WKWebViews, gestures may be off by a few pixels
    - iPadOS Multi Window apps are not YET supported.

--- SDKs/iOS/Debug/Tealeaf.framework/Headers/TLFApplicationHelper.h
+++ SDKs/iOS/Debug/Tealeaf.framework/Headers/TLFApplicationHelper.h
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2019 Acoustic, L.P. All rights reserved.
+// Copyright (C) 2021 Acoustic, L.P. All rights reserved.
 //
 // NOTICE: This file contains material that is confidential and proprietary to
 // Acoustic, L.P. and/or other developers. No license is granted under any intellectual or
@@ -9,7 +9,7 @@
 //
 #import <Foundation/Foundation.h>
 #import <WebKit/WebKit.h>
-#import "TLFPublicDefinitions.h"
+#import <Tealeaf/TLFPublicDefinitions.h>
 
 @interface TLFApplicationHelper : NSObject <TLFLibDelegate, TLFApplicationHelperJSProtocol>
 
--- SDKs/iOS/Debug/Tealeaf.framework/Headers/TLFCustomEvent.h
+++ SDKs/iOS/Debug/Tealeaf.framework/Headers/TLFCustomEvent.h
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2019 Acoustic, L.P. All rights reserved.
+// Copyright (C) 2021 Acoustic, L.P. All rights reserved.
 //
 // NOTICE: This file contains material that is confidential and proprietary to
 // Acoustic, L.P. and/or other developers. No license is granted under any intellectual or
@@ -15,7 +15,7 @@
  */
 
 #import <Foundation/Foundation.h>
-#import "TLFPublicDefinitions.h"
+#import <Tealeaf/TLFPublicDefinitions.h>
 #import <CoreLocation/CoreLocation.h>
 
 /*!
